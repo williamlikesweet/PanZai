160541b301f0e0d0ff6b31d1b1060290865b5835
KuanLauBan <williamliketina@gmail.co>
2022-08-31 15:54:32 +0800

    feature: 數字格式

--- polls/Views/WorkerView.py
+++ polls/Views/WorkerView.py
@@ -48,9 +48,9 @@ class WorkerUpdate(UpdateView):
 
 def worker_datail(request, worker_id):
     query = request.GET.get('daterangefilter')
-    workers = pd.DataFrame(Worker.objects.all().values())
-    client = pd.DataFrame(Client.objects.all().values())
-    constructionItem = pd.DataFrame(ConstructionItem.objects.all().values())
+    workers = pd.DataFrame(Worker.objects.all().values('id', 'name'))
+    client = pd.DataFrame(Client.objects.all().values('id', 'name'))
+    constructionItem = pd.DataFrame(ConstructionItem.objects.all().values('id', 'item'))
     if query:
         query = query.replace(' ', '')
         start = datetime.strptime(query.split('-', 1)[0], "%m/%d/%Y").date()
@@ -59,32 +59,35 @@ def worker_datail(request, worker_id):
         constructions = pd.DataFrame(Construction.objects.filter(worker_id=worker_id).values())
         constructions = constructions[
             (constructions['publish_at'] >= str(start)) & (constructions['publish_at'] < str(end))]
+        constructions = constructions.groupby(
+            ['publish_at', 'worker_id', 'client_id', 'work_site', 'constructionItem_id']).sum().reset_index()
     else:
         constructions = pd.DataFrame(Construction.objects.filter(worker_id=worker_id).values())
         constructions = constructions.groupby(
             ['publish_at', 'worker_id', 'client_id', 'work_site', 'constructionItem_id']).sum().reset_index()
-    resultData = pd.merge(constructions, workers, left_on="worker_id", right_on="id", how='left')
+    resultData = pd.merge(constructions, workers, left_on="worker_id", right_on="id", how='left',
+                          suffixes=('', '_worker'))
     worker_name = resultData
-    resultData = pd.merge(resultData, client, left_on="client_id", right_on="id", how='left')
-    resultData = pd.merge(resultData, constructionItem, left_on="constructionItem_id", right_on="id", how='left')
-
-    resultData = resultData.apply(lambda x: x.replace(r'\.0', "", regex=True))
+    resultData = pd.merge(resultData, client, left_on="client_id", right_on="id", how='left', suffixes=('', '_client'))
+    resultData = pd.merge(resultData, constructionItem, left_on="constructionItem_id", right_on="id", how='left',
+                          suffixes=('', '_constructionItem'))
+    # resultData = resultData.apply(lambda x: x.replace(r'\.0', "", regex=True))
     try:
         resultData = resultData.pivot_table(
-            index=['publish_at', 'name_y', 'work_site',
+            index=['publish_at', 'name_client', 'work_site',
                    'item'],
             values=['construction_amount'],
         ).unstack().replace(np.nan, '')
         resultData = resultData.droplevel(0, axis=1).reset_index()
-        resultData = resultData.replace('0', '')
         resultData['publish_at'] = resultData['publish_at'].dt.strftime('%Y-%m-%d')
         resultData = resultData.rename(
-            columns={'publish_at': '安裝日期', 'name_y': '客戶名稱', 'work_site': '案場地址'})
+            columns={'publish_at': '安裝日期', 'name_client': '客戶名稱', 'work_site': '案場地址'})
         DataFrame = resultData.to_html(table_id='example1',
-                                       classes='table table-striped table-bordered table-head-fixed text-nowrap table-hover')
-
+                                       classes='table table-striped table-bordered table-head-fixed text-nowrap table-hover',
+                                       float_format='{:10.0f}'.format
+                                       )
         return render(request, 'polls/Worker/worker_detail.html',
-                      {'resultData': resultData, 'DataFrame': DataFrame, 'worker_name': worker_name})
+                      {'resultData': resultData, 'DataFrame_html': DataFrame, 'worker_name': worker_name})
     except Exception as e:
         return render(request, '404.html', {'message': str(e)})
 
