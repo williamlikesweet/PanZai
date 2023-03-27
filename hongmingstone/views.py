000eaacbd90279ada3df257ecb2008538183df4d
KuanLauBan <williamliketina@gmail.co>
2023-03-27 14:41:30 +0800

    feature: test_login required

--- hongmingstone/views.py
+++ hongmingstone/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-
+# @login_required(login_url="Login")
 def start_document(request):
     return render(request, 'hongmingstone/starter.html')
 
